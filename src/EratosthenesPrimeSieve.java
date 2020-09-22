a23edf754cff5403ada25d93ec4e5ef966674894
BuildTools <unconfigured@null.spigotmc.org>
2020-09-22 12:50:54 +0200

    add Primes in Array

--- src/EratosthenesPrimeSieve.java
+++ src/EratosthenesPrimeSieve.java
@@ -1,5 +1,7 @@
 public class EratosthenesPrimeSieve implements PrimeSieve{
     int max;
+    int[] primes = new int[1000];
+    int primesCounter=0;
 
     public static void main(String[] args) {
         System.out.println(new EratosthenesPrimeSieve(100).isPrime(12));
@@ -20,6 +22,8 @@ public class EratosthenesPrimeSieve implements PrimeSieve{
         for (int i = 0; i < max; i++) {
             if((i % 2) != 0 && (i % 3) != 0){
                 System.out.println(i);
+                primes[primesCounter]=i;
+                primesCounter++;
             }
         }
     }
