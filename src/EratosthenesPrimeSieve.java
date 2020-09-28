beac49edd8f5fbec6731f824b7b4406055712b04
BuildTools <unconfigured@null.spigotmc.org>
2020-09-28 10:16:12 +0200

    prints out the results

--- src/EratosthenesPrimeSieve.java
+++ src/EratosthenesPrimeSieve.java
@@ -8,6 +8,7 @@ public class EratosthenesPrimeSieve implements PrimeSieve{
     public static void main(String[] args) {
         System.out.println(new EratosthenesPrimeSieve(100).isPrime(12));
         System.out.println(new EratosthenesPrimeSieve(100).isPrime(11));
+        new EratosthenesPrimeSieve(100).primeSieve();
     }
 
     public EratosthenesPrimeSieve(int max) {
@@ -24,26 +25,41 @@ public class EratosthenesPrimeSieve implements PrimeSieve{
         for (int i = 0; i < max; i++) {
             if((i % 2) != 0 && (i % 3) != 0){
                 System.out.println(i);
-                primes.add(i);
             }
         }
     }
 
     //Beispiel 2
 
+    private void listPrimes(){
+        primes.add(2);
+        primes.add(3);
+        for (int i = 3; i<max*2;i++){
+            if((i % 2) != 0 && (i % 3) != 0){
+                primes.add(i);
+            }
+        }
+    }
     @Override
     public void primeSieve() {
         printPrimes();
-        boolean[] checkVal = new boolean[max];
+        listPrimes();
+        boolean[] checkVal = new boolean[max+1];
         Arrays.fill(checkVal, true);
         for (int i = 0; i < max / 2; i++) {
             for (int j = 0; j < max / 2; j++) {
                 if(j>max)
                     break;
-                int num = primes.get(i)+primes.get(j);
-                if (num%2==0 && checkVal[num]){
-                    System.out.println(num+" = "+primes.get(i)+" + "+primes.get(j));
+                try {
+                    int num = primes.get(i) + primes.get(j);
+                    if (num%2==0 && checkVal[num]){
+                        checkVal[num] = false;
+                        System.out.println(num+" = "+primes.get(i)+" + "+primes.get(j));
+                    }
+                }catch (IndexOutOfBoundsException e){
+                    break;
                 }
+
             }
             if(i>max)
                 break;
